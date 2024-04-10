2417b18c5b31e3639d2feec18e0e591de8669225
MasonR4 <124920625+MasonR4@users.noreply.github.com>
2024-04-10 13:57:08 -0500

    getting ready for new last few uis

---
+++ menu_panels/ProfileScreen.java
@@ -0,0 +1,11 @@
+package menu_panels;
+
+import java.awt.Dimension;
+
+public class ProfileScreen {
+	
+	private static final Dimension DEFAULT_SIZE = new Dimension(1600, 900);
+	
+	
+	
+}
--- server/Client.java
+++ server/Client.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import javax.swing.SwingUtilities;
 import controller.CreateAccountScreenController;
 import controller.FindGameScreenController;
